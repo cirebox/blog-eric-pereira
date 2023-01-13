ef38381c61d617b0832a8558430c473313829bf0
Eric Pereira <eric_pereira95@hotmail.com>
2023-01-13 18:47:50 -0300

    Correção dos favicons

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -16,9 +16,9 @@ export default function BlogHeader({
     case 1:
       return (
         <header className="mt-16 mb-10 flex flex-col items-center md:mb-12 md:flex-row md:justify-between">
-          <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
+          <h2 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
             {title}
-          </h1>
+          </h2>
           <h4
             className={`mt-5 text-center text-lg md:pl-8 md:text-left ${styles.portableText}`}
           >
